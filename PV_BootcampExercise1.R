f876a0cae0b32a476f3b18d3c98e9df0c2825b47
Phuong Vo <pvo112@ucla.edu>
2016-09-14 19:39:29 -0700

    updates to exercise1 up until number 5

--- PV_BootcampExercise1.R
+++ PV_BootcampExercise1.R
@@ -58,5 +58,38 @@ for (i in 5:9){
     if(years[ii] %% 2 == 0){
       cat(years[ii], 'Hooray, congressional elections!', sep = '\t', fill = T)
     }
+    if(years [ii] %% 4 ==0){
+      cat (years[ii], 'Oh great, presidential elections...', sep ='\t', fill = T)
+    }
     
   }
+  
+  
+  #exercise 5 modify this loop so that the loop will compund the interest?
+  bankAccounts <- c(10, 9.2, 5.6, 3.7, 8.8, 0.5);
+  compounded <- rep(NA, 6)
+  interestRate <- 0.0125;
+  for (i in 1:length(bankAccounts)) {
+    compounded[i] <- interestRate*bankAccounts[i] + bankAccounts[i]; }
+  
+  compounded
+  
+  
+  #exercise 6 we now want to compound the interest annually, but across a period of 5 years. The for loop we discussed earlier only compounds for a single year. Try this:
+  
+  
+  bankAccounts <- c(10, 9.2, 5.6); #define bank accounts here
+  interestRate <- 0.0525;   
+  house <- c(4.8, 3.8, 5.7); #deduct
+  food<- c(3.5, 4.3, 5.0);    #deduct
+  fun <- c(7.8, 2.1, 10.5);  #deduct
+  #and incomes (through TAships) of 
+  income <- c(21, 21, 21); #add this
+  
+  for (j in 1:5) {
+    for (i in 1:length(bankAccounts)) {
+      #step 1 modify bankAccounts so that amounts reflect income and expenses
+      #step 2 get calculate interest and add to accounts from step 1
+      #you can actually use the line you have already written if you
+      #modify amounts in bankAccounts directly in step 1
+    }
